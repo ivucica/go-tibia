899dd0ec6f374c14757b9b779a78d814fb5167ca
Ivan Vučica <ivan@vucica.net>
2017-12-06 01:14:07 +0000

    Support external repo to items.otb in otb tests.

--- otb/otb_test.go
+++ otb/otb_test.go
@@ -9,9 +9,13 @@ func TestNewOTB(t *testing.T) {
 	f, err := os.Open(os.Getenv("GOPATH") + "/src/badc0de.net/pkg/go-tibia/datafiles/items.otb")
 	if err != nil {
 		var err2 error
-		f, err2 = os.Open(os.Getenv("TEST_SRCDIR") + "/go_tibia/datafiles/items.otb")
+		f, err2 = os.Open(os.Getenv("TEST_SRCDIR") + "/go_tibia/external/itemsotb854/file/items.otb")
 		if err2 != nil {
-			t.Fatalf("failed to open file: %s & %s", err, err2)
+			var err3 error
+			f, err3 = os.Open(os.Getenv("TEST_SRCDIR") + "/go_tibia/datafiles/items.otb")
+			if err3 != nil {
+				t.Fatalf("failed to open file: %s & %s & %s", err, err2, err3)
+			}
 		}
 	}
 	_, err = NewOTB(f)
