ce8f1badff42dd93d39c4fef29c85fa542071d79
Ivan Vučica <ivan@vucica.net>
2020-01-03 20:39:34 +0000

    otb/map: Return an error when no temples are on the map.

--- otb/map/map.go
+++ otb/map/map.go
@@ -384,6 +384,12 @@ func New(r io.ReadSeeker, t *things.Things) (*Map, error) {
 		}
 	}
 
+	if otb.defaultPlayerSpawnPoint == 0 {
+		//otb.defaultPlayerSpawnPoint = posFromCoord(44, 173, 5) // generated file
+		//otb.defaultPlayerSpawnPoint = posFromCoord(1001, 1010, 7) // test file
+		return nil, fmt.Errorf("no default player spawn point; does the map have any temples?")
+	}
+
 	return &otb, nil
 }
 
