1a6c3e9d7d8c45ffc9d0283e0b533207c8dc0bdb
Ivan Vučica <ivan@vucica.net>
2022-01-17 21:36:44 +0000

    cmd/gotwebfe: When using HTTP, perform the work in a goroutine in a promise.
    This fixes deadlocks.
    
    https://withblue.ink/2020/10/03/go-webassembly-http-requests-and-promises.html

--- cmd/gotwebfe/main.go
+++ cmd/gotwebfe/main.go
@@ -4,16 +4,19 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"image"
 	"image/png"
 	"io"
 	"log"
+	"runtime"
 	"syscall/js"
 
 	"badc0de.net/pkg/go-tibia/gameworld"
 	"badc0de.net/pkg/go-tibia/otb/map"
 	"badc0de.net/pkg/go-tibia/paths"
 	"badc0de.net/pkg/go-tibia/spr"
+	"badc0de.net/pkg/go-tibia/things"
 	"badc0de.net/pkg/go-tibia/things/full"
 	"badc0de.net/pkg/go-tibia/xmls"
 	"github.com/vincent-petithory/dataurl"
@@ -34,8 +37,17 @@ func (*console) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+var (
+	globalThings *things.Things
+	globalMap    gameworld.MapDataSource
+)
+
+const mapPath = ":test:"
+
 func main() {
 
+	flag.Set("logtostderr", "true")
+	flag.Parse()
 	log.SetOutput(&console{})
 
 	log.Println("opening tibia.spr")
@@ -63,6 +75,7 @@ func main() {
 
 	js.Global().Set("showSpr", js.FuncOf(showSpr))
 	js.Global().Set("showMap", js.FuncOf(showMap))
+	js.Global().Set("loaderPromise", js.FuncOf(loaderPromise))
 
 	// Prevent go program from exiting.
 	select {}
@@ -95,7 +108,7 @@ func showImg(in image.Image, parentElementID string, replace bool) {
 }
 func showError(pfx string, err error) {
 	log.Printf("[E] %s: %v", pfx, err)
-	
+
 	// Adding an <h1> element in the HTML document
 	document := js.Global().Get("document")
 	p := document.Call("createElement", "h1")
@@ -125,13 +138,41 @@ func showSpr(this js.Value, arg []js.Value) interface{} {
 	return nil
 }
 
-func showMap(this js.Value, arg []js.Value) interface{} {
+func loaderPromise(this js.Value, arg []js.Value) interface{} {
+	// https://withblue.ink/2020/10/03/go-webassembly-http-requests-and-promises.html
+	log.Println("constructing a loader promise")
+	defer log.Println("constructed a loader promise")
+	promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(js.FuncOf(loaderPromiseImp))
+}
+
+func loaderPromiseImp(this js.Value, arg []js.Value) interface{} {
+	resolve := arg[0]
+	reject := arg[1]
+
+	log.Print("starting the loader promise")
+	defer log.Println("started the loader promise")
+	go loaderImp(resolve, reject)
+	return nil
+}
+
+func failPromise(err error, pfx string, reject js.Value) {
+	errorConstructor := js.Global().Get("Error")
+	errorObject := errorConstructor.New(err.Error())
+
+	log.Printf("failing the promise: %v", err)
+	reject.Invoke(errorObject)
+}
+
+func loaderImp(resolve, reject js.Value) {
+	defer log.Printf("loader promise exiting")
 	log.Println("loading all from default paths")
 	t, err := full.FromDefaultPaths(true)
 	if err != nil {
-		//showError("loading required files from default paths", err)
-		return nil
+		failPromise(err, "loading required files from default paths", reject)
+		return
 	}
+	log.Println("loaded things!")
 
 	log.Println("opening outfits.xml")
 	var outfitsXML *xmls.Outfits
@@ -153,8 +194,35 @@ func showMap(this js.Value, arg []js.Value) interface{} {
 	// TODO: use outfitsXML
 	outfitsXML = outfitsXML
 
+	var m gameworld.MapDataSource
+	if mapPath == ":test:" {
+		m = gameworld.NewMapDataSource()
+	} else {
+		f, err := paths.Open(mapPath)
+		if err != nil {
+			failPromise(err, "opening map file", reject)
+			return
+		}
+		m, err = otbm.New(f, t)
+		if err != nil {
+			failPromise(err, "reading map file", reject)
+			return
+		}
+		f.Close()
+	}
+
+	globalMap = m
+	globalThings = t
+
+	log.Printf("loaded global map %p and things %p", m, t)
+
+	runtime.GC()
+
+	resolve.Invoke(nil)
+}
+
+func showMap(this js.Value, arg []js.Value) interface{} {
 	// TODO
-	mapPath := ":test:" // "map.otbm"
 	var tx, ty uint16
 	var tbot, ttop uint8
 	var tw, th int
@@ -166,23 +234,9 @@ func showMap(this js.Value, arg []js.Value) interface{} {
 	tw = 18
 	th = 14
 
-	var m gameworld.MapDataSource
-	if mapPath == ":test:" {
-		m = gameworld.NewMapDataSource()
-	} else {
-		f, err := paths.Open(mapPath)
-		if err != nil {
-			//showError("opening map file", err)
-			return nil
-		}
-		m, err = otbm.New(f, t)
-		if err != nil {
-			//showError("reading map file", err)
-			return nil
-		}
-		f.Close()
-	}
 	//log.Println("loaded, now compositing")
+	m := globalMap
+	t := globalThings
 	img := gameworld.CompositeMap(m, t, tx, ty, ttop, tbot, tw, th, 32, 32)
 	//log.Println("composited")
 
