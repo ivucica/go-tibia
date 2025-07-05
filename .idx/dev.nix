5c1539078bee4d0e709f0be4ec11242941de54f5
ivucica <ivucica@gmail.com>
2025-07-05 01:10:37 +0000

    .idx: Initial environment setup for Firebase Studio work.

---
+++ .idx/dev.nix
@@ -0,0 +1,76 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.bazelisk
+    pkgs.go
+    pkgs.clang
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+  ];
+
+  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
+  # a firebase.json file is written into the user's directory
+  services.firebase.emulators = {
+    detect = true;
+    projectId = "demo-app";
+    services = ["auth" "firestore"];
+  };
+
+  # Sets environment variables in the workspace
+  env = {
+    CC = "clang";
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "golang.go"
+      # "vscodevim.vim"
+    ];
+
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+          command = ["./run-web.sh" ];
+          manager = "web";
+           env = {
+             # Environment variables to set for your server
+              PORT = "$PORT";
+              REBUILD = "1";
+           };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install"; 
+        git-submodules = "git submodule update --init";
+
+        default.openFiles = [
+          "cmd/gotweb/gotweb.go"
+        ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
--- run-web.sh
+++ run-web.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
+
+if [[ ! -z ${IDX_ENV_CONFIG_FILE_PATH} ]] && [[ ! -z "$(command -v jq)" ]] ; then
+	WORKSPACE="$(jq -r .config.workspaceFolder "${IDX_ENV_CONFIG_FILE_PATH}")"
+	if [[ -e "${WORKSPACE}/run-web.sh" ]] ; then
+		mkdir -p "${HOME}/projects/go-tibia/src/badc0de.net/pkg"
+		ln -s "${WORKSPACE}" "${HOME}"/projects/go-tibia/src/badc0de.net/pkg/go-tibia
+	fi
+	if [[ ! -e "datafiles/Tibia.spr" ]] ; then
+		bazelisk build @tibia854//:Tibia.spr
+	fi
+fi
+
 export GOPATH="${GOPATH:-${HOME}/projects/go-tibia}"
+
 
 VAPID_ARGS=
 if [[ -e "${GOPATH}"/src/badc0de.net/pkg/go-tibia/vapid.inc.sh ]] ; then
@@ -16,4 +29,4 @@ if [[ ${WASM_BUILD} != off ]] ; then
 fi
 fi
 
-${GOPATH}/bin/gotweb --logtostderr --listen_address :9444 ${VAPID_ARGS} --flag_file=${GOPATH}/src/badc0de.net/pkg/go-tibia/gotweb.flagfile "$@"
+${GOPATH}/bin/gotweb --logtostderr --listen_address :${PORT:-9444} ${VAPID_ARGS} --flag_file=${GOPATH}/src/badc0de.net/pkg/go-tibia/gotweb.flagfile "$@"
